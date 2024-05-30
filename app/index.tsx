044f42c72d52efe6f8f25daab771129d948d7d85
Quan0308 <bddquan@gmail.com>
2024-05-30 13:41:07 +0700

    feat: add safeAreaView and scroll view

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-mono">Hello, world!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: 'blue' }}>
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-pink-50 h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="flex-1 items-center justify-center bg-white">
+          <Text className="text-3xl font-mono">Hello, world!</Text>
+          <StatusBar style="auto" />
+          <Link href="/home" style={{ color: 'blue' }}>
+            Go to Home
+          </Link>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
